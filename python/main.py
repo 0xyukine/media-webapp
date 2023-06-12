fa1c61bf5b46ff35d6880830fc45cff54ee49f64
0xyukine <44278060+0xyukine@users.noreply.github.com>
2023-06-12 17:36:00 +0100

    Minor code cleaning and file sorting for file list api get

--- python/main.py
+++ python/main.py
@@ -7,16 +7,28 @@ app = Flask(__name__)
 def test():
     return render_template('index.html')
 
+@app.route('/browser')
+def browse():
+    return "<h1>lul</h1>"
+
 @app.route('/get_images', methods=['GET'])
 def get_imageS():
+    def get_exts(x):
+        #split path and reverse elements in list
+        return os.path.splitext(x)[::-1]
+
     work_dir = os.path.dirname(os.path.realpath(__file__))
     #dir_path = os.path.join(work_dir,'static/images/')
     dir_path = 'static/images/'
     print(dir_path)
-    files = []
-    for file in os.listdir(dir_path):
-        print(file)
-        files.append(os.path.join(dir_path, file))
+    files = os.listdir(dir_path)
+    files.sort(key = get_exts)
+
+    for x in range(len(files)):
+        # files.append(os.path.join(dir_path, file))
+        files[x] = os.path.join(dir_path,files[x]) 
+
+    print(files)
     return jsonify(files)
 
 if __name__ == '__main__':
--- python/static/app.js
+++ python/static/app.js
@@ -1,22 +1,13 @@
-var mediaItems;
+var mediaItems = [];
 
 fetch('/get_images', {method: 'GET'})
     .then(response => response.json())
     .then(data => {mediaItems = data;})
     .then(data => {console.log(data)})
 
-// const mediaItems = [
-//     "images/1.jpg",
-//     "images/2.jpg",
-//     "images/3.jpg",
-//     "images/1.mp4",
-//     "images/2.mp4"
-//     ];
-
 let currentIndex = 0;
-// const imageElement = document.getElementById("currentImage");
-const mediaContainer = document.getElementById("mediaContainer")
 
+const mediaContainer = document.getElementById("mediaContainer")
 const previousButton = document.getElementById("previousButton");
 const nextButton = document.getElementById("nextButton");
 
@@ -29,44 +20,38 @@ hammer.on('swipeleft', showNextMedia);
 hammer.on('swiperight', showPreviousMedia);
 
 function showNextMedia() {
-currentIndex = (currentIndex + 1) % mediaItems.length;
-updateMedia();
+    currentIndex = (currentIndex + 1) % mediaItems.length;
+    updateMedia();
 }
 
 function showPreviousMedia() {
-currentIndex = (currentIndex - 1 + mediaItems.length) % mediaItems.length;
-updateMedia();
+    currentIndex = (currentIndex - 1 + mediaItems.length) % mediaItems.length;
+    updateMedia();
 }
 
 function updateMedia() {
-const mediaItem = mediaItems[currentIndex];
-// const {filename} = mediaItem;
+    const mediaItem = mediaItems[currentIndex];
+    mediaContainer.innerHTML = '';
 
+    const imageExtensions = ['jpg', 'jpeg', 'png'];
+    const videoExtensions = ['mp4', 'webm', 'ogg'];
+    const fileExt = getFileExt(mediaItem);
 
-mediaContainer.innerHTML = '';
-
-const imageExtensions = ['jpg', 'jpeg', 'png'];
-const videoExtensions = ['mp4', 'webm', 'ogg'];
-const fileExt = getFileExt(mediaItem);
-
-
-if (imageExtensions.includes(fileExt)) {
-const img = document.createElement('img');
-// img.src = `{{ url_for('static', filename=${mediaItem})}}`;
-// img.src = "{{ url_for('static', filename='/images/1.jpg')}}";
-img.src = `${mediaItem}`
-mediaContainer.appendChild(img);
-}
-else if (videoExtensions.includes(fileExt)) {
-const video = document.createElement('video');
-video.src = `${mediaItem}`;
-video.controls = true;
-mediaContainer.appendChild(video);
-}
+    if (imageExtensions.includes(fileExt)) {
+        const img = document.createElement('img');
+        img.src = `${mediaItem}`
+        mediaContainer.appendChild(img);
+    }
+    else if (videoExtensions.includes(fileExt)) {
+        const video = document.createElement('video');
+        video.src = `${mediaItem}`;
+        video.controls = true;
+        mediaContainer.appendChild(video);
+    }
 }
 
 function getFileExt(filename) {
-return filename.split('.').pop().toLowerCase();
+    return filename.split('.').pop().toLowerCase();
 }
 
 updateMedia();
