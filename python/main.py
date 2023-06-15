8b220809e9484de8a20ae8d61d298a1129184467
0xyukine <44278060+0xyukine@users.noreply.github.com>
2023-06-15 03:43:13 +0100

    File serving and directory traversal

--- python/main.py
+++ python/main.py
@@ -1,34 +1,63 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory, send_file
 import os
 
+file_root ='/mnt/e/Stuff/'
+
 app = Flask(__name__)
+
+class DirObject:
+    def __init__(self, obj, path):
+        self.name = obj
+        self.path = path
+        self.fp_obj = os.path.join(path, obj)
+        if os.path.isfile(self.fp_obj):
+            self.type = "file"
+            self.fp_obj = self.fp_obj
+            print(self.fp_obj)
+        elif os.path.isdir(self.fp_obj):
+            self.type = "dir"
+        else:
+            self.type = "unkown"
 
 @app.route('/')
 def test():
     return render_template('index.html')
 
-@app.route('/browser')
+@app.route('/index')
 def browse():
-    return "<h1>lul</h1>"
+    item_list = []
+    for item in os.listdir(file_root):
+        item_list.append(DirObject(item, file_root))
+    return render_template('root_index.html', item_list = item_list)
+
+@app.route('/index/<path:path>', methods=['GET'])
+def browser(path):
+    print(path)
+    item_list = []
+    path = "/" + path
+    for item in os.listdir(path):
+        item_list.append(DirObject(item, path))
+    return render_template('root_index.html', item_list = item_list)
+
+@app.route('/file/<path:filepath>')
+def get_file(filepath):
+    filepath = "/" + filepath
+    return send_file(filepath)
 
 @app.route('/get_images', methods=['GET'])
-def get_imageS():
+def get_images():
     def get_exts(x):
         #split path and reverse elements in list
         return os.path.splitext(x)[::-1]
 
-    work_dir = os.path.dirname(os.path.realpath(__file__))
-    #dir_path = os.path.join(work_dir,'static/images/')
     dir_path = 'static/images/'
-    print(dir_path)
-    files = os.listdir(dir_path)
-    files.sort(key = get_exts)
 
-    for x in range(len(files)):
-        # files.append(os.path.join(dir_path, file))
-        files[x] = os.path.join(dir_path,files[x]) 
+    #Create and sort list of files in directory
+    files = []
+    for item in os.listdir(dir_path):
+        files.append(os.path.join(dir_path, item))
+    files.sort(key=get_exts)
 
-    print(files)
     return jsonify(files)
 
 if __name__ == '__main__':
